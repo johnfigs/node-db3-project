5991234d8bb21b24c43fa0eb1c4f174eba5553a9
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-10-06 19:14:11 -0700

    wip

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require('../../data/db-config')
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,21 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const existing = await db('schemes')
+      .where('scheme_id', req.params.scheme_id)
+      .first()
 
+    if (!existing) {
+      next({ status: 404, 
+        message: `scheme with scheme_id ${scheme_id} not found`})
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
@@ -19,7 +33,15 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  const { scheme_name } = req.body
+  if (scheme_name === undefined ||
+      typeof scheme_name !== 'string' ||
+      !scheme_name.trim()
+      ) {
 
+      } else {
+        next()
+      }
 }
 
 /*
